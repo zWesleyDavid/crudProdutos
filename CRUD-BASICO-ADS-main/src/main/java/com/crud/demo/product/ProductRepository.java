a1053df342697003ba1525c15db1e86ba69b8c08
wesleydavid <123563969+zWesleyDavid@users.noreply.github.com>
2025-03-19 22:19:20 -0300

    Delete CRUD-BASICO-ADS-main directory

--- CRUD-BASICO-ADS-main/src/main/java/com/crud/demo/product/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.crud.demo.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<ProductModel, Long> {
-}
